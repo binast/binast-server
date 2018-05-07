5bc2db738fc983955193537ba2d4bb2d7149f426
Vladan Djeric <vladandjeric@gmail.com>
2018-05-07 02:16:46 -0700

    Tabs not spaces

--- convertJStoBinAST.bash
+++ convertJStoBinAST.bash
@@ -7,8 +7,8 @@ ENCODING_OPTIONS=
 
 if [ $# -lt 1 ];
 then
-   echo "Usage: convertJStoBinAST.bash <path to source directory>"
-   exit 0
+	echo "Usage: convertJStoBinAST.bash <path to source directory>"
+	exit 0
 fi
 
 for nextpath in `find $SRC_PATH -type f -name "*.js"`
