561fb83326570dbabc10e8b146c2c2334f6d418d
Vladan Djeric <vladandjeric@gmail.com>
2018-05-07 02:16:22 -0700

    Make script more flexible

--- convertJStoBinAST.bash
+++ convertJStoBinAST.bash
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-FBBENCH_PATH='../binast-server/fb_bench/'
+SRC_PATH=$1 # e.g. '../binast-server/fb_bench/'
 ENCODER_PATH="./target/debug/binjs_encode"
 
 # Note, this default is zero laziness!!
 ENCODING_OPTIONS=
 
-for nextpath in `find $FBBENCH_PATH -type f -name "*.js"`
+if [ $# -lt 1 ];
+then
+   echo "Usage: convertJStoBinAST.bash <path to source directory>"
+   exit 0
+fi
+
+for nextpath in `find $SRC_PATH -type f -name "*.js"`
 do
 	#echo $i;
 	$ENCODER_PATH $ENCODING_OPTIONS -i $nextpath -o $(dirname $nextpath) 
